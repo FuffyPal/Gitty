92c1f217396b9cc5279486ee732159c4353eed91
Flaouve <flaouve@gmail.com>
2026-06-15 10:05:42 +0300

    feat: add pushing discord webhook

--- src/webhook.go
+++ src/webhook.go
@@ -1,9 +1,13 @@
-package src
+package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type WebhookPayload struct {
@@ -25,5 +29,44 @@ func WebhookHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("**" + payload.Title + "**:")
 	fmt.Println("*" + payload.Description + "*")
 
-	w.WriteHeader(http.StatusOK)
+	sendDiscord("**" + payload.Title + "**\n" + "*" + payload.Description + "*")
+
+}
+
+func sendDiscord(message string) error {
+	webhookURL := os.Getenv("URL")
+
+	payload := map[string]string{
+		"content": message,
+	}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func main() {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("hello world")
+
+	http.HandleFunc("/webhook", WebhookHandler)
+	http.ListenAndServe(":"+port, nil)
 }
